047d50d422153945c6ee335c4caa033d9fe8eab9
moi23 <moisesboas@gmail.com>
2021-04-05 04:53:42 -0300

    feat: seed users

--- src/database/migrations/20210405073703_create_table_users.js
+++ src/database/migrations/20210405073703_create_table_users.js
@@ -1,8 +1,13 @@
+exports.up = function (knex) {
+  return knex.schema.createTable('users', function (table) {
+    table.increments('id');
+    table.text('username').unique().notNullable();
 
-exports.up = function(knex) {
-  
+    table.timestamp('created_at').defaultTo(knex.fn.now());
+    table.timestamp('updated_at').defaultTo(knex.fn.now());
+  });
 };
 
-exports.down = function(knex) {
-  
+exports.down = function (knex) {
+  return knex.schema.dropTable('users');
 };
